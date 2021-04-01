0ae3b29febfb47f18f2b510b22941d8997577aeb
Julian Bradford <bradforddeveloper@gmail.com>
2021-04-01 10:49:03 +0100

    Bug fix - Data changes sporadically on mouse hover
    var myChart now globally declared and destroy() called on each iteration of drawChart()

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,5 +1,7 @@
 // /* globals Chart:false, feather:false */
 
+var myChart;
+
 (async function () {
   'use strict'
   feather.replace()
@@ -42,10 +44,13 @@ function getChartData(covidData, buttonSelected){
 }
 
 function drawChart(covidData, buttonSelected){
-  // Graphs
   var ctx = document.getElementById('myChart')
+
+  if(window.myChart != undefined){
+    window.myChart.destroy();
+  }
   // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
+  window.myChart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: getChartLabels(covidData),
